3b5b44b083d4eb05d6cc7c477b9f4852353590c2
Sue Zheng Hao <szhlibrary@gmail.com>
2021-06-13 02:43:46 +0800

    Collections/Controller: Implement methods for collections of merchants.

---
+++ app/controllers/api/v1/collections_controller.rb
@@ -0,0 +1,57 @@
+module Api
+  module V1
+    class CollectionsController < ApplicationController
+      before_action :authenticate_user!
+      before_action :set_collection, only: [:show, :update, :destroy]
+
+      def index
+        collection_query = Collection.where(user: current_user)
+        pagy, records = pagy(collection_query)
+        render json: {
+          pagy: pagy_metadata(pagy),
+          collections: records
+        }
+      end
+
+      def create
+        @collection = Collection.new(collection_params)
+
+        if @collection.save
+          render json: @collection, status: :created
+        else
+          render json: @collection.errors, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        collection_query = @collection.merchants
+
+        pagy, records = pagy(collection_query)
+        render json: {
+          pagy: pagy_metadata(pagy),
+          collections: records
+        }
+      end
+
+      def update
+        @collection.update(collection_params)
+        render json: @collection, status: :ok
+      end
+
+      def destroy
+        @collection.destroy
+        render status: :ok
+      end
+
+      private
+
+      def set_collection
+        @collection = Collection.find(params[:id])
+      end
+
+      def collection_params
+        params.require(:collection).permit(:name)
+      end
+    end
+  end
+end
